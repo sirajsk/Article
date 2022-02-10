66fe9f7a76519ec32342e7aba12fe828f9e8ab76
sirajsk <sirajkpd12@gmail.com>
2022-02-10 13:40:22 +0530

    finished

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const userController=require('../controllers/userController')
 
 router.post('/createArticle',userController.createArticle)
-router.post('/getAllArticle',userController.getAllArticle)
+router.get('/getAllArticle',userController.getAllArticle)
 router.post('/editArticle/:id',userController.editArticle)
+router.post('/removeArticle/:id',userController.removeArticle)
+router.post('/createcategory',userController.createCategory)
+router.post('/listcategory',userController.listcategory)
 
 
 
